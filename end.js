bb3a18cb2085894ff03e9c9251b01a10a8b85901
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-08-20 17:02:54 +0100

    Update end.js
    finished highscores JavaScript storage.

--- end.js
+++ end.js
@@ -12,3 +12,23 @@ finalScore.innnerText = mostRecentScore
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value
 })
+
+saveHighScore = e => {
+    e.preventDefault()
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    }
+
+    highScores.push(score)
+
+    highScores.sort((a, b) => {
+        return b.score - a.score
+    })
+
+    highScores.splice(5)
+    
+    localStorage.setItem('highScores', JSON.stringify(highscores))
+window. location.assign('/')
+}
