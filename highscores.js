bf921d8a0268ac04c4d7a006f619f6314fcc9b02
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-08-21 17:10:05 +0100

    Update highscores.js
    changing quotes to double quotes

--- highscores.js
+++ highscores.js
@@ -4,4 +4,4 @@ const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 highScores.innerHTML =
     highScores.map(score => {
         return `<li class="high-score">${score.name} - ${score.score}</li>`
-    }).join('')
+    }).join("")
