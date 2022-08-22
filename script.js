1eda24d0e6f88b6046578276b8fd1dc52b5e5047
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-08-22 02:59:32 +0100

    Update script.js
    Removed semi colons and added missing "s" to the word questions

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
 
-let currentQuestion = {};
-let acceptingAnswers = true;
-let score = 0;
-let questionCounter = 0;
-let availableQuestions = [];
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
 
 let questions = [
     {
@@ -154,15 +154,15 @@ let questions = [
     },
 ];
 
-const SCORE_POINTS = 100;
-const MAX_QUESTIONS = 20;
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
 
 startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    getNewQuestion();
-};
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
 
 getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
@@ -170,20 +170,23 @@ getNewQuestion = () => {
 
         return window.location.assign('end.html')
     }
+
     questionCounter++
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`
 
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionIndex]
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
     question.innerText = currentQuestion.question
+
     choices.forEach(choice => {
         const number = choice.dataset['number']
         choice.innerText = currentQuestion['choice' + number]
-    });
-    availableQuestions.splice(questionIndex, 1)
+    })
 
-    acceptingAnswers = true;
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
 }
 
 choices.forEach(choice => {
@@ -194,8 +197,7 @@ choices.forEach(choice => {
         const selectedChoice = e.target
         const selectedAnswer = selectedChoice.dataset['number']
 
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :
-            'incorrect';
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
 
         if (classToApply === 'correct') {
             incrementScore(SCORE_POINTS)
@@ -214,6 +216,6 @@ choices.forEach(choice => {
 incrementScore = num => {
     score += num
     scoreText.innerText = score
-};
+}
 
-startGame();
+startGame()
